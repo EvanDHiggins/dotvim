62a0dabdc39bb6eb18cc0cb9f7c2b748e8c39a14
Evan Higgins <evan.higgins06@gmail.com>
2016-11-24 09:38:22 -0500

    Modified install.sh to build vim from source

--- install.sh
+++ install.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-git clone https://github.com/gmarik/Vundle ~/.vim/bundle/Vundle.vim
+sudo apt install tmux zsh libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev \
+    libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
+
+git clone https://github.com/vim/vim.git ${HOME}/.vim-source
+cd ${HOME}/.vim-source
+./configure --with-features=huge \
+           --enable-multibyte \
+           --enable-rubyinterp=yes \
+           --enable-pythoninterp=yes \
+           --with-python-config-dir=/usr/lib/python2.7/`ls /usr/lib/python2.7/ | grep "^config"` \
+           --enable-python3interp=yes \
+           --with-python3-config-dir=/usr/lib/python3.5/`ls /usr/lib/python3.5/ | grep "^config"` \
+           --enable-perlinterp=yes \
+           --enable-luainterp=yes \
+           --enable-gui=gtk2 --enable-cscope --prefix=${HOME}/.vim-install/
+
+sudo make install
+
+git clone https://github.com/gmarik/Vundle ${HOME}/.vim/bundle/Vundle.vim
 cd ~/.vim
 vim +PluginInstall +qall
 
